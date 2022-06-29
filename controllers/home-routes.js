a5c0dae35aff30d1f82d25049db7e51a3012adc6
Ian Hill <ianhill@Ians-MBP.home>
2022-06-29 14:35:23 -0400

    no changes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const { Gallery, Painting } = require('../models');
+const { Comment, Cookbook, Recipe, User, UserCookbook } = require('../models');
 // Import the custom middleware
-const withAuth = require('../utils/auth');
+// const withAuth = require('../utils/auth');
 
 // GET all galleries for homepage
 router.get('/', async (req, res) => {
